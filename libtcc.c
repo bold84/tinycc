99976c913758176c042db716369e3952c5fb0fef
Jarred Sumner <jarred@jarredsumner.com>
2024-09-16 23:01:05 -0700

    Add missing #ifdef

--- libtcc.c
+++ libtcc.c
@@ -2037,7 +2037,9 @@ PUB_FUNC int tcc_parse_args(TCCState *s, int *pargc, char ***pargv)
             x = TCC_OUTPUT_OBJ;
             goto set_output_type;
         case TCC_OPTION_framework:
+#ifdef TCC_TARGET_MACHO
             dynarray_add(&s->framework_names, &s->nb_framework_names, tcc_strdup(optarg));
+#endif
             break;
         case TCC_OPTION_isystem:
             tcc_add_sysinclude_path(s, optarg);
